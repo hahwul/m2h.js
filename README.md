f14c2c061b989b711e808170715235f2b4778819
hahwul <hahwul@gmail.com>
2019-02-08 00:17:33 +0900

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # m2h.js
 remote markdown document to html on DOM
+
+## Usage
+```javascript
+<script src='m2h.js'></script>
+<div id='html-area'></div>
+
+<script>
+	m2h('html-area','your remote markdown file');
+    // e.g
+    // m2h('html-area','https://raw.githubusercontent.com/hahwul/mad-metasploit/master/README.md');
+</script>
+```
---
+++ m2h.js
@@ -0,0 +1,37 @@
+try {
+    $.get()
+} catch {
+    console.log('Not use jqeury');
+    console.log('include jquery cdn');
+    var script = document.createElement("SCRIPT");
+    script.src = 'https://code.jquery.com/jquery-3.3.1.min.js';
+    script.type = 'text/javascript';
+    script.onload = function() {
+        var $ = window.jQuery;
+    };
+    document.getElementsByTagName("head")[0].appendChild(script);
+}
+
+
+// check showdown
+try {
+    var convert = new showdown.Converter()
+} catch {
+    console.log('include showdown cdn');
+    var script = document.createElement("SCRIPT");
+    script.src = 'https://rawgit.com/showdownjs/showdown/1.9.0/dist/showdown.min.js';
+    script.type = 'text/javascript';
+    script.onload = function() {
+        var convert = new showdown.Converter()
+    };
+    document.getElementsByTagName("head")[0].appendChild(script);
+}
+
+
+function m2h(node, url) {
+    var convert = new showdown.Converter()
+    zf = document.getElementById(node);
+    var a = $.get(url, function() {
+        zf.innerHTML = convert.makeHtml(a.responseText)
+    });
+}
